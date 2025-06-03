7508f84c173ebaa1a657ebfdebca4cb929fd2d24
p0lz0vatel <86878571+p0lz0vatel@users.noreply.github.com>
2025-06-03 18:34:28 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -135,7 +135,7 @@ def new_video_plot_applier(message):
     video_opened = open(video_filename, 'rb')
 
     bot.send_video(message.chat.id, video=video_opened)
-    bot.send_document(message.chat.id, video_opened, caption=f"<b>Тема Видео: {video_generator_class.topic}</b>", parse_mode="HTML")
+    bot.send_document(message.chat.id, document=video_opened, caption=f"<b>Тема Видео: {video_generator_class.topic}</b>", parse_mode="HTML")
 
   else:
     bot.send_message(message.chat.id, "Не удалось Загрузить Видео", parse_mode="MarkdownV2")
